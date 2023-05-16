45a6cfe74bdda261049c6355d7511eabb21da0ad
MartenYeager <marten.ter.meer@gmail.com>
2023-05-16 13:38:04 +0200

    Remove Newton

--- testAPI/testAPI/Models/ObjectSensorModel.cs
+++ testAPI/testAPI/Models/ObjectSensorModel.cs
@@ -3,7 +3,7 @@
     public class ObjectSensorModel
     {
         public string CurrentObject { get; set; }
-        public List <GS1Model> QRValue { get; set; }
+        public string QRValue { get; set; }
         public int Weight { get; set; }
         public int Temperature { get; set; }
         public string category { get; set; }
