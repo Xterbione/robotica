47f39cc275b204ae97a71b8ffc5d717aec4664a3
C0deSn@ke <bryan.koolman@student.nhlstenden.com>
2023-05-25 11:50:44 +0200

    manually constructing json outer brackets, inned brackets are added already in the file

--- testAPI/testAPI/Models/GS1.cs
+++ testAPI/testAPI/Models/GS1.cs
@@ -14,13 +14,13 @@ namespace testAPI.Models
         //public const string Ex3 = "{\"GS1\": [{\"Data\": \"7240Afnemercode4301I7050\"}, {\"Data\": \"7240Afnemercode4301I7041\"}, {\"Data\": \"7240Afnemercode4301I7032\"}]}";
         //public const string Ex4 = "{\"GS1\": [{\"Data\": \"7240Afnemercode4301I6860\"}, {\"Data\": \"7240Afnemercode4301E0001\"}, {\"Data\": \"7240Afnemercode4301I6803\"}, {\"Data\": \"7240Afnemercode4301E0006\"}, {\"Data\": \"7240Afnemercode4301E6318\"}, {\"Data\": \"7240Afnemercode4301E0525\"}]}";
 
-        public static List<string> getFile()
+        public static string getFile()
         {
             Console.WriteLine(DateTime.Today.ToString("yyyy-MM-dd"));
 
             string path = @"C:\Users\Public\" + currentDate + ".txt";
 
-            List<string> strings = new List<string>(File.ReadLines(path).TakeLast(5));
+            string strings = "[" + new List<string>(File.ReadLines(path).TakeLast(5)) + "]";
 
             /*if (strings.Last() == null)
             {
